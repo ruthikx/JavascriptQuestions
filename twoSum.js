bec51b82f1fb3a5b23cc98544cf3ef7d3b982ced
Ruthik M U <rruthik355@gmail.com>
2026-09-13 16:55:33 +0530

    Implement twoSum function to find indices of two numbers

---
+++ twoSum.js
@@ -0,0 +1,14 @@
+const twoSum = function(nums,target){
+    let map = new Map();
+    for(let i=0;i<nums.length;i++){
+        let rem=target-nums[i]
+        if(map.has(rem)){
+            return [map.get(rem),i]
+        }
+        map.set(nums[i],i)
+    }
+    return []
+}
+nums=[8,2,2,7]
+target=9
+console.log(twoSum(nums,target))
