42c0c3bf73fe727d6f671f5c8c20a821ce2459c6
Ruthik M U <rruthik355@gmail.com>
2026-09-20 09:41:24 +0530

    Add best time to buy and sell stock II solution

---
+++ BestTimeToBuyAndSellStockII.js
@@ -0,0 +1,21 @@
+let prices = [7,1,5,3,6,4]
+
+const maxProfit = function(prices){
+
+    let profit=0
+
+    for (let i=1;i<prices.length;i++){
+
+        if(prices[i]>prices[i-1]){
+
+            profit+=prices[i]-prices[i-1]
+
+        }
+
+    }
+
+    return profit
+
+}
+
+console.log(maxProfit(prices))
